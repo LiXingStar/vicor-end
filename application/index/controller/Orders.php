e80a7c49c849e392e2371fee8c0b718f5b5d02bb
wenstart <1459294281@qq.com>
2019-09-06 14:56:29 +0800

    '提交订单'

--- application/index/controller/Orders.php
+++ application/index/controller/Orders.php
@@ -37,34 +37,60 @@ class Orders extends Controller
     /**
      * 保存新建的资源
      *
-     * @param  \think\Request  $request
+     * @param \think\Request $request
      * @return \think\Response
      */
     public function save(Request $request)
     {
-       /*
-        *
-        * 产生订单 ->order (uid,ordernum,price,)
-        *
-        * 购物车
-        *
-        *  订单商品销量
-        * */
+        /*
+         * 产生订单 ->order (uid,ordernum,price,) -->订单商品(gid,num,uid,ordernum)
+         *
+         * 购物车
+         *
+         * 订单商品销量
+         *
+         *  */
+        Db::startTrans();
         $uid = $this->request->id;
-        $ordernum = time() . mt_rand(1000,9999);
-        $cart = Db::table('cart')->where(['uid'=>$uid])->find();
+        $ordernum = time() . mt_rand(1000, 9999);
+        $cart = Db::table('cart')->where(['uid' => $uid])->find();
         $price = $cart['price'];
         $orders = model('Ordermodel');
-        $orderResult = $orders->inserts(['uid'=>$uid,'ordernum'=>$ordernum,'price'=>$price]);
+        $orderResult = $orders->inserts(['uid' => $uid, 'ordernum' => $ordernum, 'price' => $price]);
+        // 订单商品
+        $activeGoods = Db::table('cart_extra')->field('gid,num')->where(['uid' => $uid, 'state' => 1])->select();
 
+        for ($i = 0; $i < count($activeGoods); $i++) {
+            $activeGoods[$i]['uid'] = $uid;
+            $activeGoods[$i]['ordernum'] = $ordernum;
+        }
 
-        var_dump($orderResult);
+        $orderExtraResult = Db::table('orders_extra')->insertAll($activeGoods);
+
+        // 购物车
+
+        $cartExtraResult = Db::table('cart_extra')->where(['uid'=>$uid,'state'=>1])->delete();
+
+        $cartResult =  Db::table('cart')->where(['uid'=>$uid])->update(['total'=>0,'price'=>0]);
+        if($orderResult && $orderExtraResult && $cartResult && $cartExtraResult){
+            return  json([
+                'code'=>config('code.success'),
+                'msg'=>'下单成功',
+                'data'=>$ordernum
+            ]);
+        }else{
+            return  json([
+                'code'=>config('code.fail'),
+                'msg'=>'下单失败'
+            ]);
+        }
+
     }
 
     /**
      * 显示指定的资源
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function read($id)
@@ -75,7 +101,7 @@ class Orders extends Controller
     /**
      * 显示编辑资源表单页.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function edit($id)
@@ -86,8 +112,8 @@ class Orders extends Controller
     /**
      * 保存更新的资源
      *
-     * @param  \think\Request  $request
-     * @param  int  $id
+     * @param \think\Request $request
+     * @param int $id
      * @return \think\Response
      */
     public function update(Request $request, $id)
@@ -98,7 +124,7 @@ class Orders extends Controller
     /**
      * 删除指定资源
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function delete($id)
